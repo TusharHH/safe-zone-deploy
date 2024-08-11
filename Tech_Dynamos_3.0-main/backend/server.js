de67e7bffb114c781f592efa40c89822d74681b8
Tushar <tushar.hasule@somaiya.edu>
2024-08-11 07:14:22 +0530

    clutter cleaning

--- Tech_Dynamos_3.0-main/backend/server.js
+++ Tech_Dynamos_3.0-main/backend/server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(bodyPaser.json())
 
 app.use(cors({
-    origin: ["*"],
+    origin: "*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type'],
